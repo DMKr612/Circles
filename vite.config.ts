6a0c5234eb3dbcc6da37df26be2330838e3055a3
DMKr612 <daramk80@outlook.com>
2025-10-09 21:43:37 +0200

    run it

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'              // << required for subpath hosting
+  base: '/Circles/'              // << required for subpath hosting
 })
