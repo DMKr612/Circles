1379e737df9788399886852e4a32f8bbe4ee3135
DMKr612 <daramk80@outlook.com>
2025-12-03 02:21:20 +0100

    chat problem solving ?

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
 
+// Clean up any old service workers/caches that could point to stale chunks on GH Pages
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((regs) => {
+    regs.forEach((reg) => reg.unregister());
+  }).catch(() => {});
+  if ('caches' in window) {
+    caches.keys().then((keys) => {
+      keys.forEach((k) => caches.delete(k));
+    }).catch(() => {});
+  }
+}
+
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
